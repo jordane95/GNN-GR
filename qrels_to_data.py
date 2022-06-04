53594fca86b1b941151fe23dfef1a550f3aed97b
li-zehan <zehan.li@qq.com>
2022-06-04 17:19:08 +0800

    fix bug

--- qrels_to_data.py
+++ qrels_to_data.py
@@ -4,7 +4,7 @@ import argparse
 from typing import List, Dict
 
 
-def preprocess_data(data_path: str, qrels_path: str, save_path: str):
+def preprocess_data(data_path: str, qrels_path: str, save_path: str, id2text: Dict[int, str]):
     """
     Args:
         data_path (str): path to the raw dataset
@@ -28,11 +28,6 @@ def preprocess_data(data_path: str, qrels_path: str, save_path: str):
             qrels[qid] = docids
     
     # prepare new data and save to destination path
-    # construct a id -> text dict
-    id2text: Dict[int, str] = {}
-    for data in raw_data:
-        id2text[data['id']] = data['text'][0]
-    
     for data in raw_data:
         qid = data['id']
         docids = qrels[qid] # List[int]
@@ -49,8 +44,15 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    id2text: Dict[int, str] = {}
+    with open(os.path.join(args.data_path, 'train.json'), 'r') as f:
+        train_data = json.load(f)
+    # construct a id -> text dict
+    for data in train_data:
+        id2text[data['id']] = data['text'][0]
+
     for set_name in ['train', 'dev', 'test']:
         raw_data_path = os.path.join(args.data_path, f"{set_name}.json")
         qrels_path = os.path.join(args.data_path, f"{set_name}_rank.tsv")
         save_path = os.path.join(args.data_path, f"{set_name}_gr.json")
-        preprocess_data(raw_data_path, qrels_path, save_path)
+        preprocess_data(raw_data_path, qrels_path, save_path, id2text)
